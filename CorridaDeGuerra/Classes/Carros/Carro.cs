00bb24d6aec4921d323f28fac03780a0e8a24751
Victor Augusto do Carmo <victoraugustodocarmo32@gmail.com>
2024-10-29 21:21:14 -0300

    feat: novo metodo implementado 'ReduzirVelocidadeTemporiamente'

--- CorridaDeGuerra/Classes/Carros/Carro.cs
+++ CorridaDeGuerra/Classes/Carros/Carro.cs
@@ -11,7 +11,7 @@ namespace CorridaDeGuerra.Classes.Carros
         public int Velocidade { get; protected set; }   
         public int Resistencia { get; protected set; }
         public int Posicao { get; protected set; }
-        public int Vida { get; protected set; }
+        public int Vida { get; protected set; }  
 
         protected Carro(string nome, int velocidade, int resistencia, int vida,){
             Nome = nome;
@@ -30,20 +30,29 @@ namespace CorridaDeGuerra.Classes.Carros
             Resistencia += 5;
             Console.WriteLine($"{Nome} ativou a defesa! Resistência aumentada para {Resistencia}.");
         }
-
+        
         public abstract void UsarHabilidadeEspecial(Carro oponente);
 
         public virtual void ReceberAtaque(int dano){
             int danoRecebido = Math.Max(0, dano - Resistencia);
             Vida -= danoRecebido;
             Console.WriteLine($"{Nome} foi atacado e recebeu {danoRecebido} de dano! Vida restante: {Vida}");
-        
+
             Resistencia -= 5;
         }
 
         public bool EstaNaCorrida(){
             return Vida > 0;
         }
-    
+
+        public void ReduzirVelocidadeTemporariamente(int reducao)
+        {
+            Velocidade = Math.Max(0, Velocidade - reducao);
+        }
+
+        public void RestaurarVelocidade(int aumento)
+        {
+            Velocidade += aumento;
+        }
     }
 }
